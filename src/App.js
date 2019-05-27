f15bdf686d8f26e99a647f2ae829606e6968e916
KennethJegindfeldt <jegindfeldt@gmail.com>
2019-05-27 11:22:31 +0200

    hey do

--- src/App.js
+++ src/App.js
@@ -1,26 +1,143 @@
 import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+class App extends Component {
+
+  api_url = process.env.REACT_APP_API_URL;
+
+  constructor(props) {
+      super(props);
+
+      // TODO: Move this data to the server
+      this.state = {
+          qas: [
+          ],
+
+      };
+
+      this.addQuestion = this.addQuestion.bind(this);
+      this.addAnswers = this.addAnswers.bind(this);
+
+  }
+
+
+  SOCKET_URL = `${this.api_url}/my_app`;
+
+  componentDidMount() {
+
+      const socket = io(this.SOCKET_URL);
+
+      socket.on('connect',() => {
+          console.log("Connected to Socket.IO");
+          socket.emit('Hello', "Hey" , "Dav");
+      });
+
+      socket.on('new-data', (questions) => {
+          console.log(`server msg: ${questions.msg}`);
+          this.getData();
+      });
+      this.getData();
+  }
+
+
+  async getData(){
+      fetch(`${this.api_url}/questions`)
+          .then(response => {return response.json()})
+          .then(data => this.setState({qas: data}))
+          .catch(err => console.error(err))
+  }
+
+
+  addQuestion(name, questions) {
+      fetch(`${this.api_url}/NewQuestion`, {
+          method: 'POST',
+          body: JSON.stringify({
+              name: name,
+              questions: questions
+          }),
+          headers: {
+              "Content-type": "application/json; charset=UTF-8"
+          }
+      })
+          .then(response => response.json())
+          .then(json => {
+              console.log("Result of posting a new Question");
+              console.log(json);
+          });
+  }
+
+
+
+  addAnswers(answers, id) {
+      fetch(`${this.api_url}/answers/${id}`, {
+          method: 'post',
+          body: JSON.stringify({
+              answers: answers,
+          }),
+          headers: {
+              "Content-type": "application/json; charset=UTF-8"
+          }
+      })
+          .then(response => response.json())
+          .then(json => {
+              console.log("Result of posting a new Answer");
+              console.log(json);
+          });
+  }
+
+
+
+
+  // GET QUESTION FROM ID
+  getQuestionFromId(id) {
+      console.log()
+      return this.state.qas.find((elm) => elm._id === id);
+  }
+
+  // GET ANSWERS FROM ID
+  getAnswersFromId(id) {
+      return this.state.qas.find((elm) => elm._id === id);
+  }
+
+
+  render() {
+      return (
+          <Router>
+              <div className="container">
+                  <Switch>
+                      <Route exact path={'/'}
+                             render={(props) =>
+                              <React.Fragment>
+                              <NewQuestion {...props} addQuestion={this.addQuestion}></NewQuestion>
+                                 <QuestionList {...props}
+                                       qas={this.state.qas}/>
+                              </React.Fragment>}
+                      />
+
+                      <Route exact path={'/question/:id'}
+                             render={(props) =>
+                                 <Question {...props}
+                             questions={this.getQuestionFromId(props.match.params.id) } addAnswers={this.addAnswers}   answers={this.getAnswersFromId(props.match.params.id)} />
+
+                             }
+                      />
+
+                      <Route exact path={'/NewAnswers'}
+                             render={(props) =>
+                                 <NewAnswers {...props}  addAnswers={this.addAnswers}   answers={this.getAnswersFromId(props.match.params.id)} ></NewAnswers>}/>
+
+                      <Route component={NotFound} />
+                  </Switch>
+
+              </div>
+          </Router>
+      );
+  }
 }
 
 export default App;
