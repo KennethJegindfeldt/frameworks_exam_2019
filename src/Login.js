4634e803c29503e7b67405df9a103feb419b6295
KennethJegindfeldt <jegindfeldt@gmail.com>
2019-06-05 11:03:27 +0200

    hey

--- src/Login.js
+++ src/Login.js
@@ -29,7 +29,7 @@ export default class Login extends Component {
         )
             .then(response => {
                 console.log("Authentication:", response.msg);
-                this.getData();
+                this.getJobs();
             })
             .catch(error => {
                 // TODO: Inform the user about the error
